9994059a1d6620c33762dbdcdf2f50ca24dab18a
Nattapon Pondongnok <nainatjab999@gmail.com>
2020-04-04 16:39:55 +0700

    [ADD] Init Project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Demo next.js with mysql
+
+## How to run
+
+1.copy `.env.example` to `.env` 
+
+2. edit database connection url
+3. `yarn` or `npm install` 
+4. `yarn dev` or `npm run dev` 
+5. open [localhost:3000](http://localhost:3000)
+
+## Tools
+
+* **next.js 9.3.4**
+* knext.js
+* mysql
+
---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Knex from "knex";
+
+export async function getStaticProps(context) {
+  //Connect to Database
+  const db = Knex({
+    client: "mysql",
+    connection: process.env.DB_CONNECTION_URL,
+  });
+
+  //Query table
+  const postListQuery = await db("posts").select();
+
+  //Convert result to object
+  let postList = [];
+  postListQuery.map((item) => {
+    postList.push(JSON.parse(JSON.stringify(item)));
+  });
+
+  // Send to Props
+  return { props: { postList } };
+}
+
+function HomePage({ postList }) {
+  console.log(`Post List: ${postList}`);
+  return (
+    <div>
+      <h1>Demo Post from MySQL</h1>
+      {postList &&
+        postList.map((item) => {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+    </div>
+  );
+}
+
+export default HomePage;
